7a1dd7c746f0e59550abdd6c1b07acc9b0383e3d
desainiravp <125334778+desainiravp@users.noreply.github.com>
2026-03-12 16:20:23 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,35 @@
 from flask import Flask, request, jsonify
+import psycopg2
+import os
 
 app = Flask(__name__)
 
-tasks = {
-    "tenant1": ["Deploy Kubernetes", "Setup CI/CD"],
-    "tenant2": ["Build SaaS app"]
-}
+conn = psycopg2.connect(
+    host="postgres-service",
+    database="saas",
+    user="postgres",
+    password="postgres",
+    port="5432"
+)
 
 @app.route("/tasks")
 def get_tasks():
+
     tenant = request.headers.get("tenant")
 
-    if tenant in tasks:
-        return jsonify(tasks[tenant])
+    cur = conn.cursor()
 
-    return jsonify([])
+    cur.execute("""
+        SELECT task FROM tasks
+        JOIN tenants ON tenants.id = tasks.tenant_id
+        WHERE tenants.name=%s
+    """, (tenant,))
+
+    rows = cur.fetchall()
+
+    cur.close()
+
+    return jsonify([r[0] for r in rows])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
