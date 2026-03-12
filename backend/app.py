e4b19383d141216024e9d3e7c19e3c7ad69cd4e9
nirav843 <nirav.p.desai@slscorp.com>
2026-03-12 14:44:08 +0530

    SaaS source

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+tasks = {
+    "tenant1": ["Deploy Kubernetes", "Setup CI/CD"],
+    "tenant2": ["Build SaaS app"]
+}
+
+@app.route("/tasks")
+def get_tasks():
+
+    tenant = request.headers.get("tenant")
+
+    if tenant in tasks:
+        return jsonify(tasks[tenant])
+
+    return jsonify([])
+
+app.run(host="0.0.0.0", port=5000)
